bbbf450aba98ff371e93478cb8dd74189f6907f8
Nicolas Laniau <nicolas.laniau@gmail.com>
2024-01-08 18:14:58 +0100

    Implémentation système de démo

--- backend/src/main/java/org/example/services_autoparts/ServicesAutopartsApplication.java
+++ backend/src/main/java/org/example/services_autoparts/ServicesAutopartsApplication.java
@@ -1,7 +1,9 @@
 package org.example.services_autoparts;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
 public class ServicesAutopartsApplication {
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,12 @@ import './assets/tailwind.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios";
+//axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+//axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/';
 
 const app = createApp(App)
-
+console.log(process.env.VUE_APP_API_ROOT_URL);
 app.use(router)
 
 app.mount('#app')
